38fc80d68a70b5355bb5d36bb97bc7059f9c973d
Boom-1019 <c910178@gmail.com>
2022-07-01 15:55:37 +0800

    0.1.1(20220701)fqa頁面網頁版完成

---
+++ public/js/fqa-script.js
@@ -0,0 +1,15 @@
+$('.tab-link>a').on('click',function(e){
+    e.preventDefault()
+    $(this).addClass('active')
+    $(this).siblings().removeClass('active')
+    //^上方按鈕區塊的切換^//
+
+    $($(this).attr('href')).addClass('active')
+    $($(this).attr('href')).siblings().removeClass('active')
+})//tab-link END
+
+$('.question-content').on('click', function(){
+    $(this).toggleClass('active')
+    $(this).siblings().removeClass('active')
+
+})//.question-content end
